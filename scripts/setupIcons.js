baa3afe7eb2b64234418ccc861363adf0e8888c8
Deryana Dhuzent <deredhuzent@gmail.com>
2025-10-31 20:59:08 -0700

    creating library of icons

---
+++ scripts/setupIcons.js
@@ -0,0 +1,23 @@
+// catalog of icons
+const ICONS = {
+    github: {
+        white: "/assets/icons/github_white.png",
+        // black: "/assets/images/github_black.png"
+    },
+    artstation: {
+        white: "/assets/icons/artstation_white.png",
+    }, 
+    linktree: {
+        white: "/assets/icons/linktree_white.png",
+    }
+    // instagram: {
+    //     white: "/assets/icons/instagram_white.png",
+    // },
+    // linkedIn {
+    //     white: "/assets/icons/linkedinIn_white.png",
+    // }
+}
+
+function whichIconToUse(){
+
+}
